c9f7f8aade7c3682310c3c4d9c0dadbf301a7611
asd-888 <55080464+asd-888@users.noreply.github.com>
2019-12-02 19:21:00 +0800

    框架

---
+++ call/src/App.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: 席鹏昊
+ * @Date: 2019-12-02 18:38:48
+ * @LastEditors: 席鹏昊
+ * @LastEditTime: 2019-12-02 18:44:58
+ * @Description: 
+ -->
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-size: calc(100vm / 750 * 100);
+  width: 100%;
+  height: 100%;
+}
+body {
+  width: 100%;
+  height: 100%;
+  font-size: 0.32rem;
+}
+#app{
+  width: 100%;
+  height: 100%;
+}
+</style>
